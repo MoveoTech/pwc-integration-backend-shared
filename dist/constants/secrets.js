014e7761c17aa97d545760942cd96e44488ce07f
Sean Baron <>
2022-12-29 11:53:33 +0200

    secrets naming

--- dist/constants/secrets.js
+++ dist/constants/secrets.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SECRETS = void 0;
 exports.SECRETS = {
-    MONDAY_CLIENT_SECRET: 'MONDAY_CLIENT_SECRET',
-    MONDAY_SIGNING_SECRET: 'MONDAY_SIGNING_SECRET',
-    MONDAY_TOKEN: 'MONDAY_TOKEN',
+    MONDAY_CLIENT_SECRET: 'MONDAYCLIENTSECRET',
+    MONDAY_SIGNING_SECRET: 'MONDAYSIGNINGSECRET',
+    MONDAY_TOKEN: 'MONDAYTOKEN',
 };
 //# sourceMappingURL=secrets.js.map
