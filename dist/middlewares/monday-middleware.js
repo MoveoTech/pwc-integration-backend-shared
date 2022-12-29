8632f5230f4102bfda1403cb6a58e93195ebd5f5
Sean Baron <>
2022-12-29 12:34:01 +0200

    secrets

--- dist/middlewares/monday-middleware.js
+++ dist/middlewares/monday-middleware.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.verifyClientAuthorization = exports.verifyAuthorization = void 0;
 const auth_1 = require("../utils/auth");
-const secrets_1 = require("../constants/secrets");
 const logger_service_1 = require("../services/logger-service");
 const secrets_service_1 = require("../services/secrets-service");
 const logger = logger_service_1.LoggerService.getLogger();
@@ -25,24 +24,26 @@ const verifyAuthorization = async (request, response, next) => {
             return response.status(500).send({ error: 'authorization error' });
         }
         // FROM AZURE KEY VAULT
-        const [mondaySigningSecretError, mondaySigningSecret] = await secretsService.getSecret(secrets_1.SECRETS.MONDAY_SIGNING_SECRET);
-        if (mondaySigningSecretError) {
-            logger.error({
-                message: 'missing monday signing secret',
-                fileName: 'monday middleware',
-                functionName: 'verifyClientAuthorization',
-            });
-            return response.status(500).send({ error: 'missing secret' });
-        }
-        const [monAccessTokenError, monAccessToken] = await secretsService.getSecret(secrets_1.SECRETS.MONDAY_TOKEN);
-        if (monAccessTokenError) {
-            logger.error({
-                message: 'missing monday token',
-                fileName: 'monday middleware',
-                functionName: 'verifyClientAuthorization',
-            });
-            return response.status(500).send({ error: 'missing monday token' });
-        }
+        // const [mondaySigningSecretError, mondaySigningSecret] = await secretsService.getSecret(
+        //   SECRETS.MONDAY_SIGNING_SECRET
+        // );
+        // if (mondaySigningSecretError) {
+        //   logger.error({
+        //     message: 'missing monday signing secret',
+        //     fileName: 'monday middleware',
+        //     functionName: 'verifyClientAuthorization',
+        //   });
+        //   return response.status(500).send({ error: 'missing secret' });
+        // }
+        // const [monAccessTokenError, monAccessToken] = await secretsService.getSecret(SECRETS.MONDAY_TOKEN);
+        // if (monAccessTokenError) {
+        //   logger.error({
+        //     message: 'missing monday token',
+        //     fileName: 'monday middleware',
+        //     functionName: 'verifyClientAuthorization',
+        //   });
+        //   return response.status(500).send({ error: 'missing monday token' });
+        // }
         // END FROM AZURE KEY VAULT
         // LOCAL FOR SECRET
         // const mondaySigningSecret = process.env.MONDAY_SIGNING_SECRET;
@@ -64,11 +65,11 @@ const verifyAuthorization = async (request, response, next) => {
         //   return response.status(500).send({ error: 'missing monday token' });
         // }
         // END LOCAL FOR SECRET
-        const { accountId, userId } = await (0, auth_1.verifyMondayAuthorization)(authorization, mondaySigningSecret);
+        const { accountId, userId } = await (0, auth_1.verifyMondayAuthorization)(authorization, '57a66998c1c0f6dd6dda9e19e4a953af');
         response.locals.mondayAuthorization = {
             accountId,
             userId,
-            monAccessToken,
+            monAccessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIxNDU4OTc2NCwidWlkIjozMDQyMjg0NCwiaWFkIjoiMjAyMi0xMi0yOVQwOTowNDoyNi43MDJaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTE2ODY5MDUsInJnbiI6ImV1YzEifQ._ZUJKBoJ98ycGM1rkxzGFH0RaWOU6Cx7ykxBIVbqI-k',
         };
         logger.info({
             message: 'authorization verify success',
@@ -105,24 +106,24 @@ const verifyClientAuthorization = async (request, response, next) => {
             });
             return response.status(500).send({ error: 'authorization error' });
         }
-        const [mondayClientSecretError, mondayClientSecret] = await secretsService.getSecret(secrets_1.SECRETS.MONDAY_CLIENT_SECRET);
-        if (mondayClientSecretError) {
-            logger.error({
-                message: 'missing monday client secret',
-                fileName: 'monday middleware',
-                functionName: 'verifyClientAuthorization',
-            });
-            return response.status(500).send({ error: 'missing secret' });
-        }
-        const [monAccessTokenError, monAccessToken] = await secretsService.getSecret(secrets_1.SECRETS.MONDAY_TOKEN);
-        if (monAccessTokenError) {
-            logger.error({
-                message: 'missing monday token',
-                fileName: 'monday middleware',
-                functionName: 'verifyClientAuthorization',
-            });
-            return response.status(500).send({ error: 'missing monday token' });
-        }
+        // const [mondayClientSecretError, mondayClientSecret] = await secretsService.getSecret(SECRETS.MONDAY_CLIENT_SECRET);
+        // if (mondayClientSecretError) {
+        //   logger.error({
+        //     message: 'missing monday client secret',
+        //     fileName: 'monday middleware',
+        //     functionName: 'verifyClientAuthorization',
+        //   });
+        //   return response.status(500).send({ error: 'missing secret' });
+        // }
+        // const [monAccessTokenError, monAccessToken] = await secretsService.getSecret(SECRETS.MONDAY_TOKEN);
+        // if (monAccessTokenError) {
+        //   logger.error({
+        //     message: 'missing monday token',
+        //     fileName: 'monday middleware',
+        //     functionName: 'verifyClientAuthorization',
+        //   });
+        //   return response.status(500).send({ error: 'missing monday token' });
+        // }
         // LOCAL FOR SECRET
         // const mondayClientSecret = process.env.MONDAY_CLIENT_SECRET;
         // if (!mondayClientSecret) {
@@ -143,11 +144,11 @@ const verifyClientAuthorization = async (request, response, next) => {
         //   return response.status(500).send({ error: 'missing monday token' });
         // }
         // END LOCAL FOR SECRET
-        const { dat } = await (0, auth_1.verifyMondayAuthorization)(authorization, mondayClientSecret);
+        const { dat } = await (0, auth_1.verifyMondayAuthorization)(authorization, '82de1c71c6b67adf84cd9e5ad17badf2');
         response.locals.mondayAuthorization = {
             accountId: dat.account_id,
             userId: dat.user_id,
-            monAccessToken,
+            monAccessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIxNDU4OTc2NCwidWlkIjozMDQyMjg0NCwiaWFkIjoiMjAyMi0xMi0yOVQwOTowNDoyNi43MDJaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTE2ODY5MDUsInJnbiI6ImV1YzEifQ._ZUJKBoJ98ycGM1rkxzGFH0RaWOU6Cx7ykxBIVbqI-k',
         };
         logger.info({
             message: 'authorization verify success',
