b0d19406f34e549b45b06d425744f214722ee839
Sean Baron <>
2023-01-05 10:22:19 +0200

    removed json stringify

--- dist/services/queue-service.js
+++ dist/services/queue-service.js
@@ -28,12 +28,12 @@ class QueueService {
         const credential = new identity_1.ClientSecretCredential(process.env.AZURE_TENANT_ID || '', process.env.AZURE_CLIENT_ID || '', process.env.AZURE_CLIENT_SECRET || '');
         const fullyQualifiedNamespace = `${process.env.SERVICE_BUS_NAMESPACE}.servicebus.windows.net`;
         this.client = new service_bus_1.ServiceBusClient(fullyQualifiedNamespace, credential);
-        console.log('queue client: ', JSON.stringify(this.client));
+        // console.log('queue client: ', JSON.stringify(this.client));
         return this.client;
     }
     setSender() {
         this.sender = this.client.createSender(this.queueName);
-        console.log('queue sender: ', JSON.stringify(this.sender));
+        // console.log('queue sender: ', JSON.stringify(this.sender));
         return this.sender;
     }
     setReceiver() {
@@ -44,7 +44,7 @@ class QueueService {
         }, {
             autoCompleteMessages: false,
         });
-        console.log('queue receiver: ', JSON.stringify(this.receiver));
+        // console.log('queue receiver: ', JSON.stringify(this.receiver));
         return this.receiver;
     }
     async handleMessage(message) {
